fa7c5b25ae5591db073ca14127d0fef398b433bb
Steven Humam <oppaidaisuki363@gmail.com>
2018-09-01 10:26:40 +0700

    Create kubernetesphase3.py

---
+++ kubernetesphase3.py
@@ -0,0 +1,9 @@
+#How To Use This script
+#Run python3 kubernetesphase3.py
+#You Must Done With kubernetesphase1.py and kubernetesphase2.py
+import os
+os.system("useradd kube -G sudo -m -s /bin/bash")
+os.system("passwd kube")
+os.system("sudo su kube")
+print("The Job Is Done UwU")
+print("Good Luck Have Fun")
